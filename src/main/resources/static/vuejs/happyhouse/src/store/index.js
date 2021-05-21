b4a8dce1b26a00615d7d8c09f58b55b03147008b
jejin009 <dmsine009@naver.com>
2021-05-21 17:56:12 +0900

    index

--- src/main/resources/static/vuejs/happyhouse/src/store/index.js
+++ src/main/resources/static/vuejs/happyhouse/src/store/index.js
@@ -4,8 +4,24 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    markerlocs:[]
+
+  },
+  getters: {
+    
+  },
+  mutations: {
+    ADD_MARKERLOCS(state, markerlocs) {
+      console.log('Add_Markerlocs call');
+      state.markerlocs = [];
+      state.markerlocs = markerlocs;
+    }
+  },
+  actions: {
+    addMarkerlocs({ commit }, markerlocs) {
+      commit('ADD_MARKERLOCS', markerlocs);
+    }
+  },
   modules: {},
 });

00aac720bdb5dded8ee5254657a8ee9460aea96a
nohyoonko <kny_1011@naver.com>
2021-05-21 11:05:36 +0900

    header n footer

--- src/main/resources/static/vuejs/happyhouse/src/App.vue
+++ src/main/resources/static/vuejs/happyhouse/src/App.vue
@@ -1,46 +1,39 @@
 <template>
-  <div id="app" class="container">
-   <app-header></app-header>
+  <div id="app">
+    <app-header></app-header>
     <router-view></router-view>
     <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-import AppHeader from '@/layout/Header.vue';
-import AppFooter from '@/layout/Footer.vue';
+import AppHeader from "@/layout/Header.vue";
+import AppFooter from "@/layout/Footer.vue";
 export default {
-   name: 'App',
+  name: "App",
   components: {
     AppHeader,
     AppFooter,
   },
-}
+};
 </script>
 
 <style>
 * {
-        font-family: 'Jua', sans-serif;
-        list-style: none;
-        padding: 0;
-        margin: 0;
-        text-decoration: none;
-      }
+  font-family: "Jua", sans-serif;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  text-decoration: none;
+}
 
-      .main-body {
-        height: 100%;
-        overflow: hidden;
-        background-image: url('./assets/back.jpg');
-        background-position: center;
-        background-repeat: no-repeat;
-        background-size: cover;
-        position: relative;
-      }
-
-      .navbar-nav li {
-        color: black;
-      }
-      .navbar-nav li a {
-        color: black;
-      }
+.main-body {
+  height: 100%;
+  overflow: hidden;
+  background-image: url("./assets/back.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
 </style>
--- src/main/resources/static/vuejs/happyhouse/src/layout/Footer.vue
+++ src/main/resources/static/vuejs/happyhouse/src/layout/Footer.vue
@@ -1,17 +1,15 @@
 <template>
-  <footer class="navbar navbar-expand-md justify-content-end">
-	<div class="row">
-		<div class="col-md-12">
-			<ul class="navbar-nav">
-				<li><a class="nav-link " href="#">카페소개</a></li>
-				<li><a class="nav-link" href="#">개인정보처리방침</a></li>
-				<li><a class="nav-link " href="#">이용약관</a></li>
-				<li><a class="nav-link" href="#">오시는길</a></li>
-				<li><label class="nav-link">&copy; SSAFY Corp.</label></li>
-			</ul>
-		</div>
-	</div>
-</footer>
+  <footer>
+	<b-navbar type="dark">
+		<b-navbar-nav class="ml-auto">
+			<b-nav-item href="#">카페소개</b-nav-item>
+			<b-nav-item href="#">개인정보처리방침</b-nav-item>
+			<b-nav-item href="#">이용약관</b-nav-item>
+			<b-nav-item href="#">오시는길</b-nav-item>
+			<b-nav-item>&copy; SSAFY Corp.</b-nav-item>
+		</b-navbar-nav>
+	</b-navbar>
+  </footer>
 </template>
 
 <script>
@@ -20,6 +18,8 @@ export default {
 };
 </script>
 
-<style>
-
+<style scoped>
+.navbar {
+	background-color: rgb(65, 43, 28);
+}
 </style>

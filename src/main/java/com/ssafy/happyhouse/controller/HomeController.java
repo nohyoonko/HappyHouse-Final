f0ed0d9f23b5179ebaf3da718783f94862dcd051
nohyoonko <kny_1011@naver.com>
2021-05-27 18:45:03 +0900

    modify home controller

--- src/main/java/com/ssafy/happyhouse/controller/HomeController.java
+++ src/main/java/com/ssafy/happyhouse/controller/HomeController.java
@@ -2,7 +2,6 @@ package com.ssafy.happyhouse.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
